dd3d1213fcf1f7e0dce50c4d2990dc0052b86f90
HamzaAitFeska <77283740+HamzaAitFeska@users.noreply.github.com>
2022-11-17 18:53:58 +0100

    MAS DE CLASE

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,13 +3,16 @@
 public class CameraController : MonoBehaviour
 {
     public Transform m_LookAtTransform;
-    float m_Yaw = 0.0f;
-    float m_Pitch = 0.0f;
-    public float m_Distance = 15.0f;
+    public float m_MinxDistance = 5.0f;
+    public float m_MaxDistance = 15.0f;
     public float m_YawRotationalSpeed = 720.0f;
     public float m_PitchRotationalSpeed = 720.0f;
+    public float m_Pitch = 0.0f;
     public float m_MinPitch = -60.0f;
     public float m_MaxPitch = 20.0f;
+    [Header("Avoid Obstacles")]
+    public LayerMask m_AvoidObjectsMask;
+    public float m_AvoidObjectOffset = 0.1f;
     [Header("Debug")]
     public KeyCode m_DebugLockAngleKeyCode = KeyCode.I;
     public KeyCode m_DebugLockKeyCode = KeyCode.O;
@@ -49,11 +52,30 @@ public class CameraController : MonoBehaviour
             l_MouseY = 0.0f;
         }
 #endif
-        m_Yaw += l_MouseX * m_YawRotationalSpeed * Time.deltaTime;
+        transform.LookAt(m_LookAtTransform.position);
+        float l_Distance = Vector3.Distance(transform.position, m_LookAtTransform.position);
+        l_Distance = Mathf.Clamp(l_Distance, m_MinxDistance, m_MaxDistance);
+        Vector3 l_EulerAngles = transform.rotation.eulerAngles;
+        float l_Yaw = l_EulerAngles.y;
+        
+        l_Yaw += l_MouseX * m_YawRotationalSpeed * Time.deltaTime;
         m_Pitch += l_MouseY * m_PitchRotationalSpeed * Time.deltaTime;
         m_Pitch = Mathf.Clamp(m_Pitch, m_MinPitch, m_MaxPitch);
-        Vector3 l_ForwardCamera = new Vector3(Mathf.Sin(m_Yaw * Mathf.Deg2Rad) * Mathf.Cos(m_Pitch * Mathf.Deg2Rad), Mathf.Sin(m_Pitch * Mathf.Deg2Rad), Mathf.Cos(m_Yaw * Mathf.Deg2Rad) * Mathf.Cos(m_Pitch * Mathf.Deg2Rad));
-        transform.position = m_LookAtTransform.position - l_ForwardCamera * m_Distance;
+        Vector3 l_ForwardCamera = new Vector3(Mathf.Sin(l_Yaw * Mathf.Deg2Rad) * Mathf.Cos(m_Pitch * Mathf.Deg2Rad),
+            Mathf.Sin(m_Pitch * Mathf.Deg2Rad), Mathf.Cos(l_Yaw * Mathf.Deg2Rad) * Mathf.Cos(m_Pitch * Mathf.Deg2Rad));
+        Vector3 l_DesirePosition = m_LookAtTransform.position - l_ForwardCamera * l_Distance;
+
+
+        Ray l_Ray = new Ray(m_LookAtTransform.position, -l_ForwardCamera);
+        RaycastHit l_RaycastHit;
+        if (Physics.Raycast(l_Ray,out l_RaycastHit,l_Distance,m_AvoidObjectsMask.value))
+        {
+            l_DesirePosition = l_RaycastHit.point + l_ForwardCamera * m_AvoidObjectOffset;
+        }
+
+
+        transform.position = l_DesirePosition;
         transform.LookAt(m_LookAtTransform);
+
     }
 }
--- Assets/Scripts/MarioPlayerController.cs
+++ Assets/Scripts/MarioPlayerController.cs
@@ -3,7 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 public class MarioPlayerController : MonoBehaviour
 {
+    public Camera m_Camera;
+    public float m_LerpRotation = 0.85f;
+    public float m_WalkSpeed = 2.5f;
+    public float m_RunSpeed = 6.5f;
+    [Header("Jump")]
+    public float m_VerticalSpeed;
+    public bool m_OnGround = true;
+    public float m_JumpSpeed = 5.0f;
+    public KeyCode m_JumpKeyCode = KeyCode.Space;
+    public float m_AirTime;
+
+    CharacterController m_characterController;
+    Animator m_Animator;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        m_characterController = GetComponent<CharacterController>();
+        m_Animator = GetComponent<Animator>();
+    }
     void Start()
     {
         
@@ -12,6 +30,85 @@ public class MarioPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        float l_Speed = 0.0f;
+        Vector3 l_ForwardCamera = m_Camera.transform.forward;
+        Vector3 l_RightCamera = m_Camera.transform.right;
+        l_ForwardCamera.y = 0.0f;
+        l_RightCamera.y = 0.0f;
+        l_ForwardCamera.Normalize();
+        l_RightCamera.Normalize();
+        bool l_HasMoved = false;
+
+        Vector3 l_Movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            l_HasMoved = true;
+            l_Movement = l_ForwardCamera;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            l_HasMoved = true;
+            l_Movement = -l_ForwardCamera;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            l_HasMoved = true;
+            l_Movement -= l_RightCamera;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            l_HasMoved = true;
+            l_Movement += l_RightCamera;
+        }
+
+        l_Movement.Normalize();
+
+        float l_MovementSpeed = 0.0f;
+
+        if (l_HasMoved)
+        {
+            Quaternion l_LookRotation = Quaternion.LookRotation(l_Movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, l_LookRotation, m_LerpRotation);
+
+            l_Speed = 0.5f;
+            l_MovementSpeed = m_WalkSpeed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                l_Speed = 1.0f;
+                l_MovementSpeed = m_RunSpeed;
+            }
+        }
+        m_Animator.SetFloat("Speed", l_Speed);
+        l_Movement = l_Movement * l_MovementSpeed * Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_Animator.SetTrigger("Punch");
+        }
+        m_characterController.Move(l_Movement);
+
+        m_VerticalSpeed = m_VerticalSpeed + Physics.gravity.y * Time.deltaTime;
+        l_Movement.y = m_VerticalSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(m_JumpKeyCode) && m_AirTime < 0.1f)
+        {
+            m_VerticalSpeed = m_JumpSpeed;
+            m_Animator.SetTrigger("Jump");
+        }
+            
+
+        CollisionFlags l_CollisionFlags = m_characterController.Move(l_Movement);
+        if ((l_CollisionFlags & CollisionFlags.Above) != 0 && m_VerticalSpeed > 0.0f)
+            m_VerticalSpeed = 0.0f;
+        if ((l_CollisionFlags & CollisionFlags.Below) != 0)
+        {
+            m_VerticalSpeed = 0.0f;
+            m_OnGround = true;
+            m_AirTime = 0;
+        }
+        else
+        {
+            m_AirTime += Time.deltaTime;
+            m_OnGround = false;
+        }
     }
 }
