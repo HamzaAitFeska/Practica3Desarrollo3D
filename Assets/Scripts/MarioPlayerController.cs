51bcbdbb4479e0482265b2cfc0d010ffbf2769a3
EdouAAA <ehamon@edu.tecnocampus.cat>
2022-12-06 23:33:26 +0100

    Puente arreglado

--- Assets/Scripts/MarioPlayerController.cs
+++ Assets/Scripts/MarioPlayerController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 public class MarioPlayerController : MonoBehaviour, IRestartGameElements
 {
     public enum TPunchType { RightHand, LeftHand, Kick }
